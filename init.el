9794041c195f61e5693bf9eed2b088c10a11a61d
Jacob Meixner <jacob.meixner@multiscalehn.com>
2017-01-25 12:27:34 -0800

    fix some type-face issues

--- init.el
+++ init.el
@@ -66,6 +66,10 @@
 
 ;; make sure the tree leaf faces are white
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(neo-file-link-face ((t (:foreground "White")))))
 
 ; (defun my-move-key (keymap-from keymap-to key)
