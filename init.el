be8c3cb63dc60a91a578e79609534360abde24e5
Jacob Meixner <jacob.meixner@multiscalehn.com>
2017-01-25 12:28:25 -0800

    Try to setup org mode but haven't used it

--- init.el
+++ init.el
@@ -31,7 +31,13 @@
 (setq-default indent-tabs-mode nil)
 (setq-default  tab-width 4)
 (show-paren-mode 1)
-(custom-set-variables)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/todo.txt"))))
+
  (global-auto-revert-mode 1)
 
 ;; setup to not autosave in the current dir
